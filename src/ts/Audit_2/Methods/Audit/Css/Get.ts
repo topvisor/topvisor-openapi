116da9eef222b0dab254941de53c5f80318ac0ac
Artemeey <artemeey@mail.ru>
2024-09-10 10:28:07 +0300

    Auto update shemas

--- src/ts/Audit_2/Methods/Audit/Css/Get.ts
+++ src/ts/Audit_2/Methods/Audit/Css/Get.ts
@@ -7,7 +7,10 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** Получение проверенных css */
+        /**
+         * Получение проверенных css
+         * @description test
+         */
         "Audit_2.Methods.Audit.Css.Get": {
             fields: components["schemas"]["fields"];
             orders: components["schemas"]["orders"];
