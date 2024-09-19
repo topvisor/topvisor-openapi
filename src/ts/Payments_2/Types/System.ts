0fd84c85e1685407b8ac9add26f659cb54c543fb
gitlab-runner <s1@gitlab-runner.com>
2024-09-19 20:48:25 +0300

    Auto update shemas

--- src/ts/Payments_2/Types/System.ts
+++ src/ts/Payments_2/Types/System.ts
@@ -8,7 +8,7 @@ export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
         /** @enum {string} */
-        "Payments_2.Types.System": "balance" | "card" | "card_ios" | "card_android" | "card_ap" | "card_gp" | "invoice" | "tinkoff_business" | "ap" | "ym" | "qw" | "sb" | "wm2" | "pp" | "pd" | "pd_invoice" | "rk";
+        "Payments_2.Types.System": "balance" | "card" | "card_ios" | "card_android" | "card_ap" | "card_gp" | "invoice" | "tinkoff_business" | "ap" | "ym" | "qw" | "sb" | "wm2" | "pp" | "pd" | "pd_invoice" | "rk" | "pt";
     };
     responses: never;
     parameters: never;
