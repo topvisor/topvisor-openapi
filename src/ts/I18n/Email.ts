d845a09ba3e748403f1b3b8fbdfcf3f4368612a9
gitlab-runner <gitlab-runner@s1>
2024-12-25 17:53:07 +0300

    Auto update shemas

--- src/ts/I18n/Email.ts
+++ src/ts/I18n/Email.ts
@@ -13,7 +13,7 @@ export interface components {
             /** @constant */
             User_accept_email_title?: "Account details";
             /** @constant */
-            User_accept_email_title_suffix_recommendations?: "";
+            user_accept_email_title_suffix_recommendations?: "";
             /** @constant */
             User_PWD_msg_title2?: "Password successfully changed";
             /** @constant */
