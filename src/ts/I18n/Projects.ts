c2d34441b4966c02a794232764e9dcd54b3de424
gitlab-runner <gitlab-runner@s1>
2025-02-17 10:03:43 +0300

    Auto update shemas

--- src/ts/I18n/Projects.ts
+++ src/ts/I18n/Projects.ts
@@ -470,6 +470,8 @@ export interface components {
             readonly Spent: "Spent";
             /** @constant */
             readonly Spend_limit: "Spend limit";
+            /** @constant */
+            readonly Change_url_no_available: "The project URL must match the previously configured site\nThe URL cannot be changed to an address leading to another resource.";
         };
     };
     responses: never;
