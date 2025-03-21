c73e2de875f1cb36f677528da594d9e6f1aedc64
gitlab-runner <gitlab-runner@s1>
2025-03-21 16:45:10 +0300

    Auto update shemas

--- src/ts/Topvisor.ts
+++ src/ts/Topvisor.ts
@@ -6893,7 +6893,7 @@ export interface operations {
                 content: {
                     "application/json": ({
                         result: (string | number)[];
-                    } & import('./ResponseSuccess.ts').components['schemas']['ResponseSuccess']) | import('./ResponseError.ts').components['schemas']['ResponseError'];
+                    } & import('./ResponseSuccess.ts').components['schemas']['ResponseSuccess'] & import('./Pagination.ts').components['schemas']['Pagination']) | import('./ResponseError.ts').components['schemas']['ResponseError'];
                     model: null;
                 };
             };
