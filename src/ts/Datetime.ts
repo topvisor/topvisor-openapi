238e1d03f0bb79124972f0e5f857561602433f44
Artemeey <artemeey@mail.ru>
2025-05-26 10:40:05 +0300

    Мелкие правки

--- src/ts/Datetime.ts
+++ src/ts/Datetime.ts
@@ -7,7 +7,7 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        Datetime: Record<string, never>;
+        DateTime: Record<string, never>;
     };
     responses: never;
     parameters: never;
