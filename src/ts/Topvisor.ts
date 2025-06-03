580a81e5f62a4eb413de161ebd31555736707999
gitlab-runner <gitlab-runner@s1>
2025-06-03 18:32:48 +0300

    Auto update shemas

--- src/ts/Topvisor.ts
+++ src/ts/Topvisor.ts
@@ -5498,6 +5498,23 @@ export interface paths {
         patch?: never;
         trace?: never;
     };  
+    /** Добавить регионы Google в сервис */
+    "/edit/admin_2/actions/regions/commitGoogle/": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: operations["EditAdmin2ActionsRegionsCommitGoogle"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };  
     /**
      * Обновление регионов Google
      * @description @see https://developers.google.com/google-ads/api/data/geotargets
@@ -14948,6 +14965,32 @@ export interface operations {
         requestBody: {
             content: {
                 "application/json": import('./Admin_2/Methods/Actions/Projects/Domain/Edit.ts').components['schemas']['Admin_2.Methods.Actions.Projects.Domain.Edit'];
+            };
+        };
+        responses: {
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": ({
+                        result: import('./Admin_2/Objects/Actions/Result.ts').components['schemas']['Admin_2.Objects.Actions.Result'];
+                    } & import('./ResponseSuccess.ts').components['schemas']['ResponseSuccess']) | import('./ResponseError.ts').components['schemas']['ResponseError'];
+                    model: null;
+                };
+            };
+        };
+    };
+    EditAdmin2ActionsRegionsCommitGoogle: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": import('./Admin_2/Methods/Actions/Regions/CommitGoogle/Edit.ts').components['schemas']['Admin_2.Methods.Actions.Regions.CommitGoogle.Edit'];
             };
         };
         responses: {
