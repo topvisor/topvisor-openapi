d59227b37eb31ef4fa1e8281ba4dc09e8a4d1d80
Artemeey <artemeey@mail.ru>
2025-06-06 13:31:54 +0300

    Auto update schemas

--- src/ts/OpenAPI_2/Methods/Download/Get.ts
+++ src/ts/OpenAPI_2/Methods/Download/Get.ts
@@ -12,8 +12,6 @@ export interface components {
          *
          *     В результате вернет zip файл в потоке вывода
          * @description @see https://editor.swagger.io/ Редактор Open API
-         *
-         *     test 2
          */
         "OpenAPI_2.Methods.Download.Get": {
             /**
