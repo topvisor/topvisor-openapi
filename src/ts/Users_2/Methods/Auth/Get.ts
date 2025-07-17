8a5f81f4a11100a30f3edfe911851cfa1d61fb44
gitlab-runner <gitlab-runner@s1>
2025-07-17 12:16:51 +0300

    Auto update schemas

--- src/ts/Users_2/Methods/Auth/Get.ts
+++ src/ts/Users_2/Methods/Auth/Get.ts
@@ -7,7 +7,7 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** Проверка на авторизацию пользователя */
+        /** Информация о сессии авторизации пользователя */
         "Users_2.Methods.Auth.Get": {
             /** Логин авторизации
              *
@@ -16,7 +16,10 @@ export interface components {
             /** Пароль авторизации */
             authorisation_pass?: string | null;
         };
-        "Users_2.Methods.Auth.Get.Exec": unknown[] | null;
+        "Users_2.Methods.Auth.Get.Exec": {
+            id: number;
+            auth: string;
+        } | null;
     };
     responses: never;
     parameters: never;
