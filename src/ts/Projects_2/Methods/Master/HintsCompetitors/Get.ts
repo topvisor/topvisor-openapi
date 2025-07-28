fb04fcbabf10290c9cf8accae8f62fa124295761
gitlab-runner <gitlab-runner@s1>
2025-07-28 12:16:07 +0300

    Auto update schemas

--- src/ts/Projects_2/Methods/Master/HintsCompetitors/Get.ts
+++ src/ts/Projects_2/Methods/Master/HintsCompetitors/Get.ts
@@ -13,8 +13,6 @@ export interface components {
         "Projects_2.Methods.Master.HintsCompetitors.Get": {
             /** ID проекта */
             project_id: number;
-            /** Вид получаемых подсказок конкурентов */
-            view: import('../../../../Competitors_2/Types/ByUrl/Table/View.ts').components['schemas']['Competitors_2.Types.ByUrl.Table.View'];
         };
         "Projects_2.Methods.Master.HintsCompetitors.Get.Exec": unknown[];
     };
