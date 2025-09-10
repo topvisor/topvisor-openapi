e40a5e08919a583aa81734740addf9208ca09a1b
gitlab-runner <gitlab-runner@s1>
2025-09-10 13:19:30 +0300

    Auto update schemas

--- src/ts/Admin_2/Methods/Analytics/Tickets/ChartByTags/Get.ts
+++ src/ts/Admin_2/Methods/Analytics/Tickets/ChartByTags/Get.ts
@@ -7,7 +7,7 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** Получение статистику тикетов по тегам
+        /** Получение статистики тикетов по тегам
          *
          *     @see jetbrains://idea/navigate/reference?project=topvisor-com&fqn=Services\Admin_2\Methods\Analytics\Tickets\ChartByTags\Get */
         "Admin_2.Methods.Analytics.Tickets.ChartByTags.Get": {
