1b7689a5431f35763d8303e4e0f77b0c4eea94b5
gitlab-runner <gitlab-runner@s1>
2025-09-11 17:01:29 +0300

    Auto update schemas

--- src/ts/Broker_2/Methods/Log/Get.ts
+++ src/ts/Broker_2/Methods/Log/Get.ts
@@ -19,6 +19,7 @@ export interface components {
             id?: import('../../../TV/API/Params/FiltersTrait.ts').components['schemas']['TV.API.Params.FiltersTrait']['id'];
             limit?: import('../../../TV/API/Params/LimitTrait.ts').components['schemas']['TV.API.Params.LimitTrait']['limit'];
             offset?: import('../../../TV/API/Params/OffsetTrait.ts').components['schemas']['TV.API.Params.OffsetTrait']['offset'];
+            fetch_style?: import('../../../TV/API/Params/FetchStyleTrait.ts').components['schemas']['TV.API.Params.FetchStyleTrait']['fetch_style'];
         };
         "Broker_2.Methods.Log.Get.Exec": import('../../../Models/Broker/Log.ts').components['schemas']['Models.Broker.Log'][] | null;
     };
