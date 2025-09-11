b90c8e9e171e90ff10ac4d05ed3638946e60b4dc
gitlab-runner <gitlab-runner@s1>
2025-09-11 17:55:35 +0300

    Auto update schemas

--- src/ts/I18n/Bank.ts
+++ src/ts/I18n/Bank.ts
@@ -168,8 +168,6 @@ export interface components {
             /** @constant */
             readonly plan_payment: "pricing plan subscription fee";
             /** @constant */
-            readonly Extending_tariff_failed: "Tariff failed to renew";
-            /** @constant */
             readonly Extending_subscription_failed: "Subscription failed to renew";
             /** @constant */
             readonly Extending_plan_notice: "To restart your subscription ";
