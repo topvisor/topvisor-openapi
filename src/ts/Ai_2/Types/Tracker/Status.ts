ee84d7a72e3b0a8de0f7be1a471931e3602c146a
gitlab-runner <gitlab-runner@s1>
2025-10-20 13:51:23 +0300

    Auto update schemas

--- src/ts/Ai_2/Types/Tracker/Status.ts
+++ src/ts/Ai_2/Types/Tracker/Status.ts
@@ -13,7 +13,7 @@ export interface components {
          *     @see jetbrains://idea/navigate/reference?project=topvisor-com&fqn=Services\Ai_2\Types\Tracker\Status
          * @enum {string}
          */
-        "Ai_2.Types.Tracker.Status": "default" | "start" | "check" | "result" | "reports";
+        "Ai_2.Types.Tracker.Status": "default" | "inQueue" | "checking" | "saving" | "reporting";
     };
     responses: never;
     parameters: never;
