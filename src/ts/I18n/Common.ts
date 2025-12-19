3fa8a82854def70a3f39041877077989302341bc
gitlab-runner <gitlab-runner@s1>
2025-12-19 14:23:37 +0300

    Auto update schemas

--- src/ts/I18n/Common.ts
+++ src/ts/I18n/Common.ts
@@ -150,7 +150,7 @@ export interface components {
             /** @constant */
             readonly of_project: "of project";
             /** @constant */
-            readonly Tool_digest: "Who's lying in Yandex Rank Checking?";
+            readonly Tool_digest?: "2025 year in Topvisor";
             /** @constant */
             readonly Ticket: "Ticket";
             /** @constant */
