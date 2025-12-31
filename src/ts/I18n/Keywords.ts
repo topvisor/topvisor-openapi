b7624e9c86176f1224216fc3bbd861050c7a0f2b
gitlab-runner <gitlab-runner@s1>
2025-12-31 13:51:52 +0300

    Auto update schemas

--- src/ts/I18n/Keywords.ts
+++ src/ts/I18n/Keywords.ts
@@ -621,6 +621,8 @@ export interface components {
             readonly Select_source: "Select source";
             /** @constant */
             readonly Serp_features: "SERP features";
+            /** @constant */
+            readonly No_keywords_in_project: "<a href=\"/support/keywords/import\" target=\"_blank\">Add keywords</a> distributed across groups and folders or use our <a href=\"/semantic-core/\" target=\"_blank\">keyword research tools</a>.";
         };
     };
     responses: never;
