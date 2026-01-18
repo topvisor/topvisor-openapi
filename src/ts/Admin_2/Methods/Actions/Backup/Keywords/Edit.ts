c0f02b4d6b36ecffee5470399ed68629af8d92ba
gitlab-runner <gitlab-runner@s1>
2026-01-18 12:39:23 +0300

    Auto update schemas

--- src/ts/Admin_2/Methods/Actions/Backup/Keywords/Edit.ts
+++ src/ts/Admin_2/Methods/Actions/Backup/Keywords/Edit.ts
@@ -7,7 +7,7 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** Востановление поисковых запросов из бэкапа
+        /** Восстановление поисковых запросов из бэкапа
          *
          *     @see jetbrains://idea/navigate/reference?project=topvisor-com&fqn=Services\Admin_2\Methods\Actions\Backup\Keywords\Edit */
         "Admin_2.Methods.Actions.Backup.Keywords.Edit": {
