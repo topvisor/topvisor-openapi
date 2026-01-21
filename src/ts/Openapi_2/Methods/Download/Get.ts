da5176452e3b957a951b25eb6620497b4a3dffa2
gitlab-runner <gitlab-runner@s1>
2026-01-21 20:03:16 +0300

    Auto update schemas

--- src/ts/Openapi_2/Methods/Download/Get.ts
+++ src/ts/Openapi_2/Methods/Download/Get.ts
@@ -7,11 +7,14 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
-        /** Создать Open API схемы и скачать в zip
+        /**
+         * Создать Open API схемы и скачать в zip
          *
          *     В результате вернет zip файл в потоке вывода
          *
-         *     @see jetbrains://idea/navigate/reference?project=topvisor-com&fqn=Services\Openapi_2\Methods\Download\Get */
+         *     @see jetbrains://idea/navigate/reference?project=topvisor-com&fqn=Services\Openapi_2\Methods\Download\Get
+         * @description @see https://editor.swagger.io/ Редактор Open API
+         */
         "Openapi_2.Methods.Download.Get": {
             /**
              * Формат схемы Open API, json или yaml
