c4542460833768c0e45e76d9db48091d6feffd4b
gitlab-runner <gitlab-runner@s1>
2026-03-27 17:39:16 +0300

    Auto update schemas

--- src/ts/Projects_2/Methods/Tasks/Relevants/Add.ts
+++ src/ts/Projects_2/Methods/Tasks/Relevants/Add.ts
@@ -7,6 +7,7 @@ export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
+        /** Создание задачи на проверку релевантности */
         "Projects_2.Methods.Tasks.Relevants.Add": {
             /** URL сайта */
             url: import('../../../../TV/API/Types/UrlShort.ts').components['schemas']['TV.API.Types.UrlShort'];
