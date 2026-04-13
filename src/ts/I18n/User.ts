8ae20c2260b1e829bfeee8f0733d1008e55bbe32
gitlab-runner <gitlab-runner@s1>
2026-04-13 17:41:31 +0300

    Auto update schemas

--- src/ts/I18n/User.ts
+++ src/ts/I18n/User.ts
@@ -17,7 +17,7 @@ export interface components {
             /** @constant */
             readonly password_updated: "updated";
             /** @constant */
-            readonly AITracker_alpha: "AI-tracker: Alpha version";
+            readonly AITracker_beta: "AI-tracker: Beta version";
             /** @constant */
             readonly Theme: "Тема оформления";
             /** @constant */
