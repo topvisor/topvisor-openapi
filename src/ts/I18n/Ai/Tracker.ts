7fb518d6eaaa4a1eaeb106ed3cf972316766885c
gitlab-runner <gitlab-runner@s1>
2026-04-21 16:34:55 +0300

    Auto update schemas

--- src/ts/I18n/Ai/Tracker.ts
+++ src/ts/I18n/Ai/Tracker.ts
@@ -33,7 +33,7 @@ export interface components {
             /** @constant */
             readonly System_prompt: "User context";
             /** @constant */
-            readonly System_prompt_placeholder: "Enter additional information to help you fine-tune the context.";
+            readonly System_prompt_placeholder: "Enter additional information to help you fine-tune the context";
             /** @constant */
             readonly Automatic_pick_up_of_alternative_brand_spellings: "Automatic pick up of alternative brand spellings";
             /** @constant */
