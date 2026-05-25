0beb3de04a7c43c29a36f2e88f2c043705d5d9ac
gitlab-runner <gitlab-runner@s1>
2026-05-25 21:11:56 +0300

    Auto update schemas

--- src/ts/AiTracker_2/Methods/History/Export/Get.ts
+++ src/ts/AiTracker_2/Methods/History/Export/Get.ts
@@ -46,6 +46,12 @@ export interface components {
             } | null;
             history_fields: import('../../../Types/History/HistoryFields.ts').components['schemas']['AiTracker_2.Types.History.HistoryFields'];
             models_keys: unknown;
+            /**
+             * Фильтрация по тональности на последнюю дату периода
+             *
+             *     Игнорируется, если выбрано несколько моделей, так как для фильтрации нужен конкретный `model_key`
+             */
+            filter_by_sentiment?: import('../../../Types/History/FilterBySentiments.ts').components['schemas']['AiTracker_2.Types.History.FilterBySentiments'] | null;
             /** При фильтрации по ID папок также искать в подпапках */
             group_folder_id_depth?: boolean;
             /** Показывать удаленные запросы */
--- src/ts/AiTracker_2/Methods/History/Get.ts
+++ src/ts/AiTracker_2/Methods/History/Get.ts
@@ -15,6 +15,12 @@ export interface components {
             show_exists_dates?: boolean;
             history_fields: import('../../Types/History/HistoryFields.ts').components['schemas']['AiTracker_2.Types.History.HistoryFields'];
             models_keys: unknown;
+            /**
+             * Фильтрация по тональности на последнюю дату периода
+             *
+             *     Игнорируется, если выбрано несколько моделей, так как для фильтрации нужен конкретный `model_key`
+             */
+            filter_by_sentiment?: import('../../Types/History/FilterBySentiments.ts').components['schemas']['AiTracker_2.Types.History.FilterBySentiments'] | null;
             /** При фильтрации по ID папок также искать в подпапках */
             group_folder_id_depth?: boolean;
             /** Показывать удаленные запросы */
