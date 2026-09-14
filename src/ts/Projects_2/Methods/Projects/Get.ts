cbf12326039df59cdf0fe8fc075aafca26c4c791
gitlab-runner <gitlab-runner@s1>
2026-09-14 08:20:53 +0300

    Auto update schemas

--- src/ts/Projects_2/Methods/Projects/Get.ts
+++ src/ts/Projects_2/Methods/Projects/Get.ts
@@ -66,7 +66,7 @@ export interface components {
              */
             offset?: number;
         };
-        "Projects_2.Methods.Projects.Get.Exec": import('../../../Models/Projects.ts').components['schemas']['Models.Projects'][];
+        "Projects_2.Methods.Projects.Get.Exec": import('../../../Models/Projects.ts').components['schemas']['Models.Projects'][] | null;
     };
     responses: never;
     parameters: never;
